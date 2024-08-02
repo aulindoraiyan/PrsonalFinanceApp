b00a4628bd759d2b197380f37a31c924c10ca0ce
Raiyan Mahfuz Aulindo <aulindo.raiyan1107@gmail.com>
2024-08-02 02:48:17 -0400

    1st commit

---
+++ server/src/schema/financial-record.ts
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+interface FinancialRecord {
+  userId: string;
+  date: Date;
+  description: string;
+  amount: number;
+  category: string;
+  paymentMethod: string;
+}
+
+// const financialRecordSchema = new mongoose.Schema<FinancialRecord>({
+//   userId: { type: String, required: true },
+//   date: { type: Date, required: true },
+//   description: { type: String, required: true },
+//   amount: { type: Number, required: true },
+//   category: { type: String, required: true },
+//   paymentMethod: { type: String, required: true },
+// });
+const financialRecordSchema = new mongoose.Schema<FinancialRecord>(
+    {
+      userId: { type: String, required: true },
+      date: { type: Date, required: true },
+      description: { type: String, required: true },
+      amount: { type: Number, required: true },
+      category: { type: String, required: true },
+      paymentMethod: { type: String, required: true },
+    },
+    { collection: 'financial_records' } // Specify the collection name explicitly
+  );
+  
+
+const FinancialRecordModel = mongoose.model<FinancialRecord>(
+  "FinancialRecord",
+  financialRecordSchema
+);
+
+export default FinancialRecordModel;
